e4b453357f17c27c32ebc687b550a68fc6184ab3
IsraelChidera <israelchidera54@gmail.com>
2023-01-17 21:40:09 +0100

    Project upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Documentation;
+
+namespace Documentation_client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            DocumentDescription documentation = new();
+            documentation.GetDocs();
+        }
+    }
+}
