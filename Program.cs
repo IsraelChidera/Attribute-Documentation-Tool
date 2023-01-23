6cf6ee1ee32291e045a5db9ed52a54e5cdda4262
IsraelChidera <israelchidera54@gmail.com>
2023-01-23 11:18:52 +0100

    Feature Added: Serialization, Deserialization, File Streaming

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using Documentation;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
 
 namespace Documentation_client
 {
     internal class Program
     {
+
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine();
-            DocumentDescription documentation = new();
-            documentation.GetDocs();
+            //DocumentDescription.WriteDocsToTxtFile();
+            //DocumentDescription.GetDocsTextFile();            
+            JsonConverters.Generate();
         }
     }
 }
